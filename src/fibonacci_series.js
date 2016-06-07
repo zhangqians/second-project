e75cdf865424d494b78137ba50e4c111fb2d6217
zhangqians <2545026281@qq.com>
2016-06-07 10:06:54 +0800

    Update fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -2,34 +2,7 @@
 
 function fibonacci_series(n) 
 {
-	var arr=[0];
-	var arrs=[0,1];
-	var arrss=[0,1,1];
-	
-	
-		if(n==0)
-		{
-			return arr;
-			}
-		else if(n==1)
-		{
-			return arrs;
-			}
-		else if(n==2)
-		{
-		return arrss;
-			}
-		else
-		{
-		for(var i=3;i<n+1;i++)
-		{
-			
-			arrss[i]=2*arrss[i-1]-arrss[i-3];
-			arrss.push(arrss[i]);
-		}
-		return arrss;
-			}
-		
+
 		
      
 }
